e502eb64ec659cf6862f16e9af678bc2eeafa6e2
DEEPAK KUMAR <98394284+thedeepak2601@users.noreply.github.com>
2024-07-10 14:40:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# thedeepak2601.github.io
-This is my personal portfolio. I am a full Stack Developer. I was born  in Muzaffarpur, Bihar and raised in Lucknow, Uttar Pradesh. I have a lot of enthusiasm for life and am always looking for new opportunities to learn and grow.
+# Personal-Portfolio_Website
---
+++ js/script.js
@@ -0,0 +1,42 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
